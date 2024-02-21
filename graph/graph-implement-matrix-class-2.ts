578dac07400537f2af3d0e80ea41df2a48519bc8
Bishal Giri <giribsaal@gmail.com>
2024-02-21 18:07:18 +0900

    - add directed graph bfs traversal

--- graph/graph-implement-matrix-class-2.ts
+++ graph/graph-implement-matrix-class-2.ts
@@ -7,6 +7,7 @@ export default class Graph {
     size: number;
     vertices: Array<string> = [];
     matrix: Array<Array<number>> = [];
+    visited = new Map<number, boolean>;
 
     private initializeMatrix() {
         this.matrix = Array(this.size).fill(0).map(each => Array(this.size).fill(0));
@@ -42,17 +43,41 @@ export default class Graph {
         }
         console.log(']');
     }
+
+    dfsUtil(root: number) {
+        this.visited.set(root, true);
+        console.log(this.vertices[root]);
+
+        for (let i = 0; i < this.size; i++) {
+            if (this.matrix[root][i] && !this.visited.has(i)) this.dfsUtil(i);
+        }
+    }
+
+    dfsTraversal(root: string) {
+        const rootIndex = this.vertices.indexOf(root);
+
+        this.dfsUtil(rootIndex);
+    }
 }
 
-const graph = new Graph(4);
-graph.addVertice(0, 'A');
-graph.addVertice(1, 'B');
-graph.addVertice(2, 'C');
-graph.addVertice(3, 'D');
+const graph = new Graph(7);
+
+graph.addVertice(0, 'A')
+graph.addVertice(1, 'B')
+graph.addVertice(2, 'C')
+graph.addVertice(3, 'D')
+graph.addVertice(4, 'E')
+graph.addVertice(5, 'F')
+graph.addVertice(6, 'G')
 console.log('Vertices: ', graph.vertices);
 
-graph.addEdge(0, 1, 3);
-graph.addEdge(0, 2, 2);
-graph.addEdge(2, 1, 1);
-graph.addEdge(3, 0, 4);
+graph.addEdge(3, 0, 1)  // D -> A
+graph.addEdge(3, 4, 1)  // D -> E
+graph.addEdge(4, 0, 1)  // E -> A
+graph.addEdge(0, 2, 1)  // A -> C
+graph.addEdge(2, 5, 1)  // C -> F
+graph.addEdge(2, 6, 1)  // C -> G
+graph.addEdge(5, 1, 1)  // F -> B
+graph.addEdge(1, 2, 1)  // B -> C
 graph.printGraph();
+graph.dfsTraversal('D');
